91a3273210a6eb1faf6bb7676ffd4730e4400f8d
juli0r <julian@syrenia.de>
2020-06-05 21:48:25 +0200

    style fixes - commented out not finished modules

--- src/app/modules/pitch-detect/pitch-detect.component.ts
+++ src/app/modules/pitch-detect/pitch-detect.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-pitch-detect',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PitchDetectComponent implements OnInit {
 
+  @ViewChild("NoteC", { static: false }) NoteC: HTMLAudioElement;
+  @ViewChild("NoteCs", { static: false }) NoteCs: HTMLAudioElement;
+  @ViewChild("NoteD", { static: false }) NoteD: HTMLAudioElement;
+  @ViewChild("NoteDs", { static: false }) NoteDs: HTMLAudioElement;
+  @ViewChild("NoteE", { static: false }) NoteE: HTMLAudioElement;
+  @ViewChild("NoteF", { static: false }) NoteF: HTMLAudioElement;
+  @ViewChild("NoteFs", { static: false }) NoteFs: HTMLAudioElement;
+  @ViewChild("NoteG", { static: false }) NoteG: HTMLAudioElement;
+  @ViewChild("NoteGs", { static: false }) NoteGs: HTMLAudioElement;
+  @ViewChild("NoteA", { static: false }) NoteA: HTMLAudioElement;
+  @ViewChild("NoteAs", { static: false }) NoteAs: HTMLAudioElement;
+  @ViewChild("NoteB", { static: false }) NoteB: HTMLAudioElement;
+  @ViewChild("NoteC2", { static: false }) NoteC2: HTMLAudioElement;
+  @ViewChild("NoteCs2", { static: false }) NoteCs2: HTMLAudioElement;
+  @ViewChild("NoteD2", { static: false }) NoteD2: HTMLAudioElement;
+  @ViewChild("NoteDs2", { static: false }) NoteDs2: HTMLAudioElement;
+  @ViewChild("NoteE2", { static: false }) NoteE2: HTMLAudioElement;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  playNote(event: Event) {
+    var pianoKey = event.target as HTMLDivElement;
+    var audio = pianoKey.children[0] as HTMLAudioElement;
+    audio.play();
+  }
 }
